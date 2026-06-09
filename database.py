04ebd7d7d2a46dc7399491f8b7580ab101ab53cf
IvanDevyatkin <ivan.devyatkin11@gmail.com>
2026-06-10 01:28:20 +0200

    Use environment variable for database URL

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
 # the file configures the db connection for the app
 
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "postgresql://ivandevyatkin@localhost:5432/todo_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://ivandevyatkin@localhost:5432/todo_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
